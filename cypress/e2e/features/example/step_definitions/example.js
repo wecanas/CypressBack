f200b43e32099cba59715de5f0a3efdf5326fe96
wecanas <61715701+wecanas@users.noreply.github.com>
2025-09-04 02:10:53 -0600

    [FEATURE]back-and-front

--- cypress/e2e/features/example/step_definitions/example.js
+++
@@ -1,9 +0,0 @@
-import { Given, Then } from '@badeball/cypress-cucumber-preprocessor';
-
-Given('I open the Cypress website', () => {
-  cy.visit('https://www.cypress.io');
-});
-
-Then('I should see "JavaScript End to End Testing Framework"', () => {
-  cy.contains('JavaScript End to End Testing Framework').should('be.visible');
-});
--- cypress/e2e/features/front/example.feature
+++ cypress/e2e/features/front/example.feature
@@ -2,3 +2,6 @@ Feature: Example feature for Gherkin
   Scenario: Visit the Cypress website
     Given I open the Cypress website
     Then I should see "JavaScript End to End Testing Framework"
+
+  Scenario: Another example scenario
+    Given I am on the home page
